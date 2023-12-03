b3e8859294550ba8423f40ce0423c3d91ca1b895
Kanish Kumar <kanishkumar11@outlook.in>
2023-12-03 19:52:42 +0530

    Initial commit

---
+++ src/appwrite/config.js
@@ -0,0 +1,109 @@
+import conf from "../conf/conf.js";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.createDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (err) {
+      return err;
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (err) {
+      return err;
+    }
+  }
+  async deletPost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (err) {
+      return err;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (err) {
+      return err;
+    }
+  }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (err) {
+      return err;
+    }
+  }
+  // File Upload Method
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (err) {
+      return err;
+    }
+  }
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (err) {
+      return err;
+    }
+  }
+  async getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
+}
+const service = new Service();
+export default service;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Signup as signupcomp } from "../components";
+const Signup = () => {
+  return (
+    <div className="py-8">
+      <signupcomp />
+    </div>
+  );
+};
+
+export default Signup;
